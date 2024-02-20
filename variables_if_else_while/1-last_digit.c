aed1340169ab7285c02e09b7a3814753a235feee
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-20 15:31:56 +0100

    Task1 Print if the last digit of a random number is greater than 5, less than 6 and not 0, or is 0

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/*Include stdio.h to use printf function*/
+#include <stdio.h>
+/**
+* main - Entry point
+* This program will assign a random number to the variable n
+* each time it is executed.
+* It prints the last digit of n,
+* and says it is greather than 5, less than 6 or equal to 0
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if ((n % 10) > 5) /*n > 5*/
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+
+	else
+		if ((n % 10) == 0)  /*case n == 0*/
+			printf("Last digit of %d is %d and is 0\n", n, n % 10);
+		else /* case n < 6 ans n!= 0 */
+			printf("Last digit of %d is %d and is less than 6 and is not 0\n",
+			 n, n % 10);
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 Task0 'Print if a random number is positive, negative or equal to zero'
+Task1 'Print if the last digit of a random number is greater than 5, less than 6 and not 0, or is 0'
