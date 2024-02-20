b6a348f17d030eb484036385402b40ae0b916eff
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-20 14:53:14 +0100

    Task 0 - First program using if else statements by printing if an interger is positive, negative or null

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,31 +4,29 @@
 /*Include stdio.h to use printf function*/
 #include <stdio.h>
 
-/* betty style doc for function main goes there
- *
- * This program will assign a random number to the variable n each time it is executed.
- * It prints whether the number stored in the variable n is positive or negative.
+/**
+* main - Entry point
+* This program will assign a random number to the variable n
+* each time it is executed.
+* It prints whether the number stored in the variable n is
+* positive or negative.
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/*case n positive*/
 	if (n > 0)
-		{
-			printf("%d is positive\n",n);
-		}
-	else if (n < 0)
-			{
-				printf("%d is negative\n",n);
-			}
-		else /* n==0 */
-			{
-				printf("%d is zero\n",n);
-			}
+		printf("%d is positive\n", n);
+
+	else
+		if (n < 0)  /*case n negative*/
+			printf("%d is negative\n", n);
+		else /* case n==0 */
+			printf("%d is zero\n", n);
 
 	return (0);
 }
