b92c903999173ce06061393eff18a4e14c9bc8c3
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-20 18:50:30 +0100

    Task2 Print the alphabet followed by a new line by only using twice the function putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,8 @@ int main(void)
 	/*Put the alphabet in the array tabl_alpha*/
 	char tabl_alpha[27] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for( i = 0; i < 27; i++)
+
+	for (i = 0 ; i < 27 ; i++)
 		putchar(tabl_alpha[i]);
 	/*Print a new line*/
 	putchar('\n');
