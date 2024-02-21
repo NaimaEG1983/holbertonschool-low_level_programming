80813aa7e611164e74d859c937ef7a100efd0ed1
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-21 01:00:03 +0100

    Print numbers from 0 to f in base hexa

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Print numbers from 0 to 9 followed by ', '
+ * by using 4 times the putchar function
+ * with no declaration of type char
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	/*Print from ASCII value of 0 and increment it*/
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	/*Print a new line*/
+	putchar('\n');
+
+	return (0);
+}
