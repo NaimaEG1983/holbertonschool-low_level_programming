86f83191e36246231c06de4adca16caa2396cbe5
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-21 22:53:15 +0100

    Task0 write a program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+/*#include main.h*/
+/**
+ * main - Entry point
+ * program that prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+ /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	int i;
+	char tableC[9];
+
+	tableC = "_putchar";
+
+	for (i = 0 ; i < 9 ; i++)
+		_putchar(tableC[i]);
+
+	/*Print a new line*/
+	_putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Task0 */
+int _putchar(char c);
+
+#endif MAIN_H
