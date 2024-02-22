69f0abd76080e3a9b2e59b2a3e5f1e4404068993
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-22 12:08:39 +0100

    Task 0 - Write a program that prints _putchar, followed by a new line

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,5 @@
-#include <unistd.h>
-#include main.h
+#include "main.h"
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
  * main - Entry point
  * program that prints _putchar, followed by a new line
@@ -27,14 +15,23 @@ int _putchar(char c)
 
 int main(void)
 {
-	int i;
-	char tableC[9];
 
-	tableC = "_putchar";
+	/*char tableC[9];
+	*
+	*tableC = "_putchar";
+	*int i;
 
-	for (i = 0 ; i < 9 ; i++)
-		_putchar(tableC[i]);
-
+	*for (i = 0 ; i < 8 ; i++)
+	*	_putchar(tableC[i]);
+	*/
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	/*Print a new line*/
 	_putchar('\n');
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
+
 
 /* Task0 */
 int _putchar(char c);
 
-#endif MAIN_H
