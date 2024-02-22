c767c298f24989b39d2773bacf09b31cf8b9e9be
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-22 12:20:02 +0100

    Task 0 - Write a program that prints _putchar, followed by a new line

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,13 +5,6 @@
  * program that prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
- /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 
 int main(void)
 {
