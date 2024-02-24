9b8001bd69704e3f1b95f5758509ecf7c48906fd
NaimaEG1983 <8923@holbertonstudents.com>
2024-02-24 19:44:40 +0100

    Task15 - print the sum of even-values of Fibonacci numbers which are under 4000000

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - print the sum of even-values of first Fibonacci numbers under 4000000
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long n1, n2, sum, sumP;
+
+	n1 = 1;
+	n2 = 2;
+	sumP  = n2;
+	
+	do	{
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+		if (!(n2 % 2))
+			sumP += n2;
+
+	} while (n2 < 4000000);
+
+	printf("%lu\n", sumP);
+	return (0);
+}
