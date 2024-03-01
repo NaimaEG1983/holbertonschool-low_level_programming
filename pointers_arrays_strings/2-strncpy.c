49e3ef0cb8175691822b9b091efe99723d9f6424
NaimaEG1983 <8923@holbertonstudents.com>
2024-03-01 16:36:43 +0100

    Task2 - copy a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,22 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 0, l;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	l = i - 1;
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
-
 	dest[i] = '\0';
 
+	if (n > l)
+	{
+		for (i = l; i < n; i++)
+			dest[i] = '\0';
+	}
 	return (dest);
 }
